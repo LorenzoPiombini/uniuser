5b3eb5aeddbbd0c05432f813e5acb408a1348658
LorenzoPiombini <piombinilorenzo1987@gmail.com>
2025-03-06 11:50:41 -0500

    cleaned #inclued statments in main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <pwd.h>
-#include <errno.h>
 #include "user_create.h"
 
 int main(int arg, char** argv)
